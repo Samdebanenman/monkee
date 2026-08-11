0049f7114f7240db6c9eddccac49220f0da3f4f5
Samdebanenman <254315690+Samdebanenman@users.noreply.github.com>
2026-08-11 05:14:13 +0200

    cleanup

--- services/incompleteCoopsService.js
+++
@@ -1,7 +0,0 @@
-import { listIncompleteCoops } from '../utils/database/incompleteCoopsRepository.js';
-
-export function fetchIncompleteCoops() {
-  return listIncompleteCoops();
-}
-
-export default { fetchIncompleteCoops };
--- utils/database/index.js
+++ utils/database/index.js
@@ -5,7 +5,6 @@ export * from './contractsRepository.js';
 export * from './colleggtiblesRepository.js';
 export * from './coopsRepository.js';
 export * from './contractHistoryRepository.js';
-export * from './incompleteCoopsRepository.js';
 export * from './membersRepository.js';
 
 export { default } from './client.js';
